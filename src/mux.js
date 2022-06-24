0c90d0f12897bfde8af70c92e5dc70e2e112ea55
Omri Zohar <omri.zohar@gmail.com>
2022-06-24 17:12:28 -0400

    Unit tests for storage

--- src/mux.js
+++ src/mux.js
@@ -1,12 +1,14 @@
 // Main http routing controller.
-const redirector = require('./redirector')
+const redirector = require('./redirector');
+const Redirector = new redirector.Redirector();
+
 async function mux(req, res) {
     if (req.url === "/healthz" && req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text/text" });
         res.write("ok");
         res.end();
     } else {
-        redirector.redirect(req,res);
+        Redirector.redirect(req,res);
     }
 }
 
---
+++ test/storage.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var storage = require('../src/storage')
+describe('Storage tests', function () {
+  describe('#get()', function () {
+    it('google', function () {
+      let s = new storage.Storage();
+      assert.equal(s.get('google'), 'https://www.google.com')
+    });
+it('non-exist', function () {
+      let s = new storage.Storage();
+      assert.equal(s.get('lkjadlasdl'), null)
+    });
+  });
+  describe('#set()', function () {
+    it('dummy', function () {
+      let s = new storage.Storage();
+      s.set('aaa', 'bbb');
+      assert.equal(s.get('aaa'), 'bbb')
+    });
+  });
+});
